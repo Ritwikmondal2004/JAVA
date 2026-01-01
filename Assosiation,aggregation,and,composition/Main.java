9775cf4ca932f719b9497ee3e549dab5d31198b1
Ritwikmondal2004 <ritwik8575@gmail.com>
2026-01-01 16:43:43 +0530

    many to many

--- Assosiation,aggregation,and,composition/Main.java
+++ Assosiation,aggregation,and,composition/Main.java
@@ -1,3 +1,4 @@
+import java.text.ListFormat.Style;
 import java.util.*;
 //one to one relation seep
 // Passport class
@@ -31,44 +32,90 @@ import java.util.*;
 //         student.displayDetails();
 //     }
 // }
-class Student {
-    private String name;
-    private int id;
-    public Student(String name,int id){
-        this.name=name;
-        this.id=id;
 
+// one to many relation seep
+// class Student {
+//     private String name;
+//     private int id;
+//     public Student(String name,int id){
+//         this.name=name;
+//         this.id=id;
+
+//     }
+//     public String getName(){
+//         return name;
+//     }
+//     public int getid(){
+//         return id;
+//     }
+// }
+// class College{
+//     private String name;
+//     private List<Student> students;
+//     public College(String name,List<Student> students){
+//         this.name=name;
+//         this.students=students;
+//     }
+//     public void  displayDetails(){
+//         System.out.println("College Name:"+name);
+//         for(int i=0;i<students.size();i++){
+//             Student student=students.get(i);
+//             System.out.println("Student Name:"+student.getName()+"id--"+student.getid());
+//         }
+//     }
+// }
+// public class Main{
+//     public static void main(String [] args){
+//         List<Student> students=new ArrayList<>();
+//         students.add(new Student("Ritwik Kumar Mondal", 101));
+//         students.add(new Student("Amit Kumar Mondal", 102));
+//         students.add(new Student("Rohit Kumar Mondal", 103));
+//         students.add(new Student("Amit Kumar Mondal", 104));
+//         College college=new College("UIT College",students);
+//         college.displayDetails();
+//     }
+// }
+
+//composition relation seep
+class Student{
+    private String name;
+    List<Student> courses;
+    public Student(String name,List<Student>courses){
+        this.name=name;
+        this.courses=new ArrayList<>();
+    }
+    public void addCourse(Student courseName){
+        courses.add(courseName);
     }
     public String getName(){
         return name;
     }
-    public int getid(){
-        return id;
-    }
 }
-class College{
-    private String name;
-    private List<Student> students;
-    public College(String name,List<Student> students){
-        this.name=name;
-        this.students=students;
+class Course{
+    private String courseName;
+    List<Course> students;
+    public Course(String courseName){
+        this.courseName=courseName;
+        this.students=new ArrayList<>();
     }
-    public void  displayDetails(){
-        System.out.println("College Name:"+name);
-        for(int i=0;i<students.size();i++){
-            Student student=students.get(i);
-            System.out.println("Student Name:"+student.getName()+"id--"+student.getid());
-        }
+    public void addStudent(Course studentName){
+        students.add(studentName);
+    }
+    public String getCourseName(){
+        return courseName;
     }
 }
 public class Main{
     public static void main(String [] args){
-        List<Student> students=new ArrayList<>();
-        students.add(new Student("Ritwik Kumar Mondal", 101));
-        students.add(new Student("Amit Kumar Mondal", 102));
-        students.add(new Student("Rohit Kumar Mondal", 103));
-        students.add(new Student("Amit Kumar Mondal", 104));
-        College college=new College("UIT College",students);
-        college.displayDetails();
+        Student student1=new Student("Ritwik Kumar Mondal",null);
+        Student student2=new Student("Amit Kumar Mondal",null);
+        Course course1=new Course("Data Structure");
+        Course course2=new Course("Algorithm");
+        course1.addStudent(course2);
+        student1.addCourse(student2);
+        System.out.println("Student Name:"+student1.getName());
+        System.out.println("Course Name:"+course1.getCourseName());
+        System.out.println("Student Name:"+student2.getName());
+        System.out.println("Course Name:"+course2.getCourseName());
     }
 }
